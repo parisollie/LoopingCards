2302bed7fea1bac3d9de79d5fca0ceb68259671c
parisollie <pjff18@gmail.com>
2025-03-27 11:39:11 -0600

    Initial Commit

---
+++ LoopingCards/LoopingCardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoopingCardsApp.swift
+//  LoopingCards
+//
+//  Created by Paul F on 27/03/25.
+//
+
+import SwiftUI
+
+@main
+struct LoopingCardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
